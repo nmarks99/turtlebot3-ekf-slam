4f6b29a9ab425acede038ff5b040be819e7c24d4
Nick Marks <nmarks99@gmail.com>
2023-01-10 15:31:00 -0600

    tested overloaded '<<' for for Vector2D:

--- turtlelib/src/main.cpp
+++ turtlelib/src/main.cpp
@@ -3,27 +3,12 @@
 #include <cmath>
 
 int main(int argc, char * argv[]) {
+    
+    turtlelib::Vector2D v;
+    v.x = 2.0;
+    v.y = 3.0;
 
-
-    static_assert(turtlelib::almost_equal(0, 0), "is_zero failed");
-
-    static_assert(turtlelib::almost_equal(turtlelib::deg2rad(0.0), 0.0), "deg2rad failed");
-
-    static_assert(turtlelib::almost_equal(turtlelib::rad2deg(0.0), 0.0), "rad2deg) failed");
-
-    static_assert(
-            turtlelib::almost_equal(
-                turtlelib::deg2rad(turtlelib::rad2deg(2.1)),
-                2.1
-            ),
-            "deg2rad failed"
-    );
-
-    std::cout <<
-        "1.57rad = " <<
-        ceil(turtlelib::rad2deg(1.57)) <<
-        "deg" <<
-    std::endl;
+    std::cout << v << std::endl;
 
     return 0;
 }
