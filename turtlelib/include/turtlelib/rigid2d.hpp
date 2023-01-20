acae9b51ff6852f4310b105b027f2bb6f6367582
Nick Marks <nmarks99@gmail.com>
2023-01-19 23:23:37 -0600

    remove matt's directions in comment

--- turtlelib/include/turtlelib/rigid2d.hpp
+++ turtlelib/include/turtlelib/rigid2d.hpp
@@ -88,20 +88,6 @@ namespace turtlelib
     ///   [x y] or x y
     /// \param is - stream from which to read
     /// \param v [out] - output vector
-    ///
-    /// The way input works is (more or less): what the user types is stored in a buffer until the user types
-    /// a newline (by pressing enter).  The iostream methods then process the data in this buffer character by character.
-    /// Typically, each character is examined and then removed from the buffer automatically.
-    /// If the characters don't match what is expected (e.g., we are expecting an int but the letter 'q' is encountered)
-    /// an error flag is set on the stream object (e.g., std::cin) and processing stops.
-    ///
-    /// We have lower level control however.
-    /// std::peek() looks at the next unprocessed character in the buffer without removing it
-    ///     https://en.cppreference.com/w/cpp/io/basic_istream/peek
-    /// std::get() removes the next unprocessed character from the buffer.
-    ///     https://en.cppreference.com/w/cpp/io/basic_istream/get
-    /// When you call std::peek() it will wait for there to be at least one character in the buffer (e.g., the user types a character)
-    /// HINT: this function can be written in under 20 lines and uses only std::peek(), std::get(), istream::operator>>() and a little logic
     std::istream & operator>>(std::istream & is, Vector2D & v);
 
 
@@ -119,17 +105,18 @@ namespace turtlelib
 
     };
 
-    /// \brief output a 2 dimensional twist as [xcomponent ycomponent]
+    /// \brief output a 2 dimensional twist as [thetadot xdot ydot]
     /// os - stream to output to
     /// v - the twist to print
     std::ostream &operator<<(std::ostream &os, const Twist2D &v);
 
-    /// \brief input a 2 dimensional vector
+    /// \brief input a 2 dimensional twist 
     ///   You should be able to read vectors entered as follows:
-    ///   [x y] or x y
+    ///   [thetadot xdot ydot] or thetadot xdot ydot
     /// \param is - stream from which to read
     /// \param v [out] - output vector
     std::istream &operator>>(std::istream &is, Twist2D &v);
+
 
     /// \brief a rigid body transformation in 2 dimensions
     class Transform2D
