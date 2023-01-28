cb063b8a2858d664cad7b056d866122b9bf56300
Nick Marks <nmarks99@gmail.com>
2023-01-28 17:11:31 -0600

    add normalize_angle function and tests

--- turtlelib/include/turtlelib/rigid2d.hpp
+++ turtlelib/include/turtlelib/rigid2d.hpp
@@ -3,22 +3,18 @@
 /// \file
 /// \brief Two-dimensional rigid body transformations.
 
-#include <iosfwd>    // contains forward definitions for iostream objects
-#include <cstdlib>  // C standard library
+#include <iosfwd>  // contains forward definitions for iostream objects
+#include <cstdlib> // C standard library
 #include <vector>
 #include <cmath>
 #include <iostream>
 #include <cassert>
 
-
 namespace turtlelib
 {
 
-    /// \brief Matrix: alias for std::vector<std::vector<double>>
-    using Matrix = std::vector<std::vector<double>>;
-
     /// \brief PI.  Not in C++ standard until C++20.
-    constexpr double PI=3.14159265358979323846;
+    constexpr double PI = 3.14159265358979323846;
 
     /// \brief approximately compare two floating-point numbers using
     ///        an absolute comparison
@@ -29,14 +25,28 @@ namespace turtlelib
     /// NOTE: implement this in the header file
     /// constexpr means that the function can be computed at compile time
     /// if given a compile-time constant as input
-    constexpr bool almost_equal(double d1, double d2, double epsilon=1.0e-12)
+    constexpr bool almost_equal(double d1, double d2, double epsilon = 1.0e-12)
     {
-        if (fabs(d1 - d2) < epsilon) {
+        if (fabs(d1 - d2) < epsilon)
+        {
             return true;
-        } 
-        else {
+        }
+        else
+        {
             return false;
         }
+    }
+
+    /// \brief Normalizes angle to be in the interval (-pi,pi]
+    /// \param rad - an angle in radians
+    /// \return angle (radians) in (-pi,pi]
+    constexpr double normalize_angle(double rad)
+    {
+        if (turtlelib::almost_equal(-M_PI, rad))
+        {
+            return M_PI;
+        }
+        return atan2(sin(rad), cos(rad));
     }
 
     /// \brief convert degrees to radians
@@ -44,7 +54,7 @@ namespace turtlelib
     /// \returns radians
     constexpr double deg2rad(double deg)
     {
-        double rad = deg * (PI/180);
+        double rad = deg * (PI / 180);
         return rad;
     }
 
@@ -53,7 +63,7 @@ namespace turtlelib
     /// \returns the angle in degrees
     constexpr double rad2deg(double rad)
     {
-        double deg = rad * (180/PI);
+        double deg = rad * (180 / PI);
         return deg;
     }
 
@@ -69,7 +79,6 @@ namespace turtlelib
 
     static_assert(almost_equal(deg2rad(rad2deg(2.1)), 2.1), "deg2rad failed");
 
-
     /// \brief A 2-Dimensional Vector
     struct Vector2D
     {
@@ -82,25 +91,22 @@ namespace turtlelib
         /// \brief computes the L2 norm of the 2D vector
         /// \return the normalized vector
         Vector2D normalize() const;
-
     };
-
 
     /// \brief output a 2 dimensional vector as [xcomponent ycomponent]
     /// os - stream to output to
     /// v - the vector to print
-    std::ostream & operator<<(std::ostream & os, const Vector2D & v);
+    std::ostream &operator<<(std::ostream &os, const Vector2D &v);
 
     /// \brief input a 2 dimensional vector
     ///   You should be able to read vectors entered as follows:
     ///   [x y] or x y
     /// \param is - stream from which to read
     /// \param v [out] - output vector
-    std::istream & operator>>(std::istream & is, Vector2D & v);
-
+    std::istream &operator>>(std::istream &is, Vector2D &v);
 
     /// \brief A 2-Dimensional Twist
-    struct Twist2D 
+    struct Twist2D
     {
         /// \brief angular velocity
         double thetadot = 0.0;
@@ -110,7 +116,6 @@ namespace turtlelib
 
         /// \brief y velocity
         double ydot = 0.0;
-
     };
 
     /// \brief output a 2 dimensional twist as [thetadot xdot ydot]
@@ -118,26 +123,23 @@ namespace turtlelib
     /// v - the twist to print
     std::ostream &operator<<(std::ostream &os, const Twist2D &v);
 
-    /// \brief input a 2 dimensional twist 
+    /// \brief input a 2 dimensional twist
     ///   You should be able to read vectors entered as follows:
     ///   [thetadot xdot ydot] or thetadot xdot ydot
     /// \param is - stream from which to read
     /// \param v [out] - output vector
     std::istream &operator>>(std::istream &is, Twist2D &v);
 
-
     /// \brief a rigid body transformation in 2 dimensions
     class Transform2D
     {
 
     private:
-        
         double angle;
-        Vector2D p_vec; 
+        Vector2D p_vec;
         // std::vector<std::vector<double>> tf_vec; // probably not needed
 
     public:
-        
         /// \brief Create an identity transformation
         Transform2D();
 
@@ -161,14 +163,14 @@ namespace turtlelib
         Vector2D operator()(Vector2D v) const;
 
         /// \brief invert the transformation
-        /// \return the inverse transformation. 
+        /// \return the inverse transformation.
         Transform2D inv() const;
 
-        /// \brief compose this transform with another and store the result 
+        /// \brief compose this transform with another and store the result
         /// in this object
         /// \param rhs - the first transform to apply
         /// \return a reference to the newly transformed operator
-        Transform2D & operator*=(const Transform2D & rhs);
+        Transform2D &operator*=(const Transform2D &rhs);
 
         /// \brief the translational component of the transform
         /// \return the x,y translation
@@ -178,44 +180,42 @@ namespace turtlelib
         /// \return the angular displacement, in radians
         double rotation() const;
 
-        /// \brief use the adjoint to find the representation of 
+        /// \brief use the adjoint to find the representation of
         /// the given twist in another reference frame specified
         /// by the transform described by the Transform2D object (T_ij)
         /// \param V - a twist represented the j frame
         /// \return a twist represented in the i frame
-        Twist2D map_twist(Twist2D V) const; 
+        Twist2D map_twist(Twist2D V) const;
 
         /// \brief \see operator<<(...) (declared outside this class)
         /// for a description
-        friend std::ostream & operator<<(std::ostream & os, const Transform2D & tf);
-        
+        friend std::ostream &operator<<(std::ostream &os, const Transform2D &tf);
+
         /// \brief \see operator>>(...) (declared outside this class)
         /// for a description
-        friend std::istream & operator>>(std::istream & is, Transform2D & tf);
-
+        friend std::istream &operator>>(std::istream &is, Transform2D &tf);
     };
-
 
     /// \brief should print a human readable version of the transform:
     /// An example output:
     /// deg: 90 x: 3 y: 5
     /// \param os - an output stream
     /// \param tf - the transform to print
-    std::ostream & operator<<(std::ostream & os, const Transform2D & tf);
+    std::ostream &operator<<(std::ostream &os, const Transform2D &tf);
 
     /// \brief Read a transformation from stdin
     /// Should be able to read input either as output by operator<< or
     /// as 3 numbers (degrees, dx, dy) separated by spaces or newlines
     /// For example:
     /// 90 2 3
-    std::istream & operator>>(std::istream & is, Transform2D & tf);
+    std::istream &operator>>(std::istream &is, Transform2D &tf);
 
     /// \brief multiply two transforms together, returning their composition
     /// \param lhs - the left hand operand
     /// \param rhs - the right hand operand
     /// \return the composition of the two transforms
     /// HINT: This function should be implemented in terms of *=
-    Transform2D operator*(Transform2D lhs, const Transform2D & rhs);
+    Transform2D operator*(Transform2D lhs, const Transform2D &rhs);
 
 }
 
