ed0bcb560fd716ae24965df6ca761d5cce7a50c8
Nick Marks <nmarks99@gmail.com>
2023-02-02 17:39:57 -0600

    got the build to work finding the turtlelib headers

--- nuturtle_control/src/turtle_control.cpp
+++ nuturtle_control/src/turtle_control.cpp
@@ -9,6 +9,8 @@
 #include "rclcpp/logging.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "nuturtlebot_msgs/msg/wheel_commands.hpp"
+#include "turtlelib/diff_drive.hpp"
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
@@ -27,6 +29,9 @@ public:
         "cmd_vel", 10,
         std::bind(&NuturtleControl::cmd_vel_callback, this, _1));
 
+    /// @brief Publisher to wheel_cmd topic
+    _wheel_cmd_pub = this->create_publisher<nuturtlebot_msgs::msg::WheelCommands>("wheel_cmd", 10);
+
     /// @brief Timer
     _timer = this->create_wall_timer(500ms, std::bind(&NuturtleControl::timer_callback, this));
     count = 0;
@@ -36,9 +41,11 @@ private:
   uint16_t count;
   rclcpp::TimerBase::SharedPtr _timer;
   rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr _cmd_vel_sub;
+  rclcpp::Publisher<nuturtlebot_msgs::msg::WheelCommands>::SharedPtr _wheel_cmd_pub;
 
   void cmd_vel_callback(const geometry_msgs::msg::Twist &V)
   {
+    // TODO: DiffDrive::inverse_kinematics(V) to get wheel speeds
     RCLCPP_INFO_STREAM(this->get_logger(), "Vx = " << V.linear.x);
     RCLCPP_INFO_STREAM(this->get_logger(), "Vy = " << V.linear.y);
   }
