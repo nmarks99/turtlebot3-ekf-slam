c9a1f24b951786429e69621ed91e7af64e7ef8e7
Nick Marks <nmarks99@gmail.com>
2023-02-02 19:03:55 -0600

    subscription to cmd_vel and publshing to wheel_cmd works as expected

--- nuturtle_control/src/turtle_control.cpp
+++ nuturtle_control/src/turtle_control.cpp
@@ -16,6 +16,9 @@ using namespace std::chrono_literals;
 using std::placeholders::_1;
 using std::placeholders::_2;
 
+const double WHEEL_RADIUS = 0.066;
+const double WHEEL_SEPARATION = 0.160;
+
 /// \brief nusim turtlebot simulation node
 class NuturtleControl : public rclcpp::Node
 {
@@ -35,6 +38,8 @@ public:
     /// @brief Timer
     _timer = this->create_wall_timer(500ms, std::bind(&NuturtleControl::timer_callback, this));
     count = 0;
+
+    turtlelib::DiffDrive turtlebot(WHEEL_RADIUS, WHEEL_SEPARATION);
   }
 
 private:
@@ -42,17 +47,30 @@ private:
   rclcpp::TimerBase::SharedPtr _timer;
   rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr _cmd_vel_sub;
   rclcpp::Publisher<nuturtlebot_msgs::msg::WheelCommands>::SharedPtr _wheel_cmd_pub;
+  turtlelib::DiffDrive turtlebot;
 
-  void cmd_vel_callback(const geometry_msgs::msg::Twist &V)
+  void cmd_vel_callback(const geometry_msgs::msg::Twist &Vmsg)
   {
-    // TODO: DiffDrive::inverse_kinematics(V) to get wheel speeds
-    RCLCPP_INFO_STREAM(this->get_logger(), "Vx = " << V.linear.x);
-    RCLCPP_INFO_STREAM(this->get_logger(), "Vy = " << V.linear.y);
+    // Convert the geometry_msg/Twist to a turtlelib/Twist2D
+    turtlelib::Twist2D V;
+    V.xdot = Vmsg.linear.x;
+    V.ydot = Vmsg.linear.y;
+    V.thetadot = Vmsg.angular.z;
+
+    // Inverse kinematics to get the required wheel speeds
+    turtlelib::WheelState speeds = turtlebot.inverse_kinematics(V);
+
+    // Publish wheel speeds to wheel_cmd topic
+    nuturtlebot_msgs::msg::WheelCommands wheel_cmd_msg;
+    wheel_cmd_msg.left_velocity = speeds.left;
+    wheel_cmd_msg.right_velocity = speeds.right;
+    _wheel_cmd_pub->publish(wheel_cmd_msg);
+
+    //
   }
 
   void timer_callback()
   {
-    RCLCPP_INFO_STREAM(this->get_logger(), "Hi " << count);
     count++;
   }
 };
